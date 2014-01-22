db733f4f8b720f47a47f583454f3d744393fc68c
Ashwin Valento <ashwinvalento@gmail.com>
2014-01-22 09:22:45 +0530

    Changed database to SQLite . working code. Proxy Server enabled.

--- src/run/DBConnect.java
+++ src/run/DBConnect.java
@@ -9,27 +9,41 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Administrator
  */
 public final class DBConnect {
-    // JDBC driver name and database URL
-    //Mysql Drivers
 
-    public static String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    public static String DB_URL = "jdbc:derby://localhost:1527/FetchResults";
-    static final String USER = "root";
-    static final String PASS = "root";
+    // JDBC driver name and database URL
+    //SQLITE database Drivers
+    public static String driver = "org.sqlite.JDBC";
+    public static String DB_URL = "jdbc:sqlite:FetchResults.sqlite";
+
+    //ApaChe derby Embeded drivers
+    /*
+     public String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+     // public static String DB_URL = "jdbc:derby://localhost:1527/FetchResults";
+     public static String DB_URL = "jdbc:derby:FetchResults;create=true";
+     static final String USER = "root1";
+     static final String PASS = "roo1t";
+     */
     public static Connection connection = null;
 
     public static void getConnection() {
 
         try {
-            connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            Class.forName(driver);
+            //connection for embeded derby
+            // connection = DriverManager.getConnection(DB_URL, USER, PASS);
+            //connection for SQLITE
+            connection = DriverManager.getConnection(DB_URL);
             System.out.println("Connected to database...");
         } catch (SQLException ex) {
+            Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
             Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
 

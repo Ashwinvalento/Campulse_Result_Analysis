74de5417b1a5eb899374b364af7a1ad3aa0142ba
Ashwin Valento <ashwinvalento@gmail.com>
2014-02-06 03:33:02 +0530

    -> Updated to reflect DB change -> auto retry limit code added ->Infinite auto retry bug removed (i think :P )

--- src/Main/DownloadMarksTask.java
+++ src/Main/DownloadMarksTask.java
@@ -15,28 +15,25 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 import run.DBConnect;
+import run.DBInterface;
 
-public class DownloadMarksTask extends SwingWorker<Integer, Integer> {
+public class DownloadMarksTask extends SwingWorker<Integer, Integer> implements DBInterface {
 
-    PreparedStatement pstmt, pstmtMain;
+    PreparedStatement pstmtSub, pstmtStud;
 
     public DownloadMarksTask() {
         //initialize 
         Connection con = DBConnect.connection;
 
-        // String sql = " DELETE from RESULTTABLE ";
-        //String query = "INSERT INTO RESULTTABLE values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-        String query = "Insert into sub_table values (?,?,?,?,?,?)";
-        String queryMain = "Insert into main values (?,?,?,?)";
+        String subQuery = "Insert into " + SUBJECT_DETAILS + " values (?,?,?,?,?,?)";
+        String stdQuery = "Insert into " + STUDENT_DETAILS + " values (?,?,?,?)";
         try {
-            pstmt = con.prepareStatement(query);
-            System.out.println("pstmt");
+            pstmtSub = con.prepareStatement(subQuery);
         } catch (SQLException ex) {
             Logger.getLogger(DownloadMarksTask.class.getName()).log(Level.SEVERE, null, ex);
         }
         try {
-            pstmtMain = con.prepareStatement(queryMain);
-            System.out.println("pstmtMain");
+            pstmtStud = con.prepareStatement(stdQuery);
         } catch (SQLException ex) {
             Logger.getLogger(DownloadMarksTask.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -45,7 +42,6 @@ public class DownloadMarksTask extends SwingWorker<Integer, Integer> {
 
     @Override
     public Integer doInBackground() throws InterruptedException {
-        MainForm.curUsnDownloadLabel.show();
         resultFetch r = new resultFetch();
         if (MainForm.stopFlag == true) {
             stopFetching();
@@ -54,43 +50,29 @@ public class DownloadMarksTask extends SwingWorker<Integer, Integer> {
             for (int i = 0; i < MainForm.usnList.size() && MainForm.stopFlag == false; i++) {
                 int colCount = 1;
                 setProgress((i + 1) * 100 / MainForm.usnList.size());
-                MainForm.setCurStatusLabel("Downloading USN :" + MainForm.usnList.get(i));
                 System.out.println(MainForm.usnList.get(i));
 
-                if ((r.FetchTheresult(MainForm.usnList.get(i), EnterUsnForm.sem))) { //&& r.fetchSubjectNames(MainForm.usnList.get(i), EnterUsnForm.sem)) {
-                    //display marks
-
-                    //      pstmt.setString(colCount++, MainForm.usnList.get(i));
-                    //     pstmt.setString(colCount++, r.name);
-                    pstmtMain.setString(1, MainForm.usnList.get(i));
-                    pstmtMain.setString(2, r.name);
-                    pstmtMain.setInt(3, Integer.parseInt(r.totalmarks));
-                    pstmtMain.setString(4, r.mk);
-                    pstmtMain.execute();
-                    pstmt.setString(1, MainForm.usnList.get(i));
+                if (r.FetchTheresult(MainForm.usnList.get(i), EnterUsnForm.sem)) {
+                    pstmtStud.setString(1, MainForm.usnList.get(i));
+                    pstmtStud.setString(2, r.name);
+                    pstmtStud.setInt(3, Integer.parseInt(r.totalmarks));
+                    pstmtStud.setString(4, r.mk);
+                    pstmtStud.execute();
+                    pstmtSub.setString(1, MainForm.usnList.get(i));
                     for (int x = 0; x < 8; x++) {
-                        //         pstmt.setInt(colCount++, r.marks[x][1]);
-                        //         pstmt.setInt(colCount++, r.marks[x][0]);
-                        //         pstmt.setInt(colCount++, r.marks[x][2]);
-                        //         pstmt.setString(colCount++, r.res[x]);
-
-                        pstmt.setString(2, r.subjects[x]);
-                        pstmt.setInt(3, r.marks[x][1]);
-                        pstmt.setInt(4, r.marks[x][0]);
-                        pstmt.setInt(5, r.marks[x][2]);
-                        pstmt.setString(6, r.res[x]);
-                        pstmt.execute();
+                        pstmtSub.setString(2, r.subjects[x]);
+                        pstmtSub.setInt(3, r.marks[x][1]);
+                        pstmtSub.setInt(4, r.marks[x][0]);
+                        pstmtSub.setInt(5, r.marks[x][2]);
+                        pstmtSub.setString(6, r.res[x]);
+                        pstmtSub.execute();
                     }
-                    //        pstmt.setInt(colCount++, Integer.parseInt(r.totalmarks));
-                    //        pstmt.setString(colCount++, r.mk);
-
                 }
             }
 
         } catch (Exception e) {
             System.out.println("Error:" + e);
             JOptionPane.showMessageDialog(null, e + " Download marks task");
-            System.out.println("Exception is here");
         }
         return 1;
     }
@@ -102,14 +84,14 @@ public class DownloadMarksTask extends SwingWorker<Integer, Integer> {
 
     @Override
     protected void done() {
-        // MainForm.objCopy.usnProgressBar.hide();
-        MainForm.objCopy.clickStop();
-        System.out.println("DONE!!!");
-        MainForm.log("Download Completed.");
-        MainForm.log("Please check download details to see list of USN failed to fetch.");
-        MainForm.setCurStatusLabel("DOWNLOAD COMPLETE");
-        if (MainForm.autoRetry && MainForm.stopFlag == false) {
+        if (MainForm.autoRetry && MainForm.retrylimit>0 && !MainForm.RetryList.isEmpty() && MainForm.stopFlag == false ) {
             MainForm.objCopy.clickRestart();
+            MainForm.retrylimit--;
+        } else {
+            System.out.println("DONE!!!");
+            MainForm.log("Download Completed.");
+            MainForm.log("Please check download details to see list of USN failed to fetch.");
+            MainForm.objCopy.clickStop();
         }
 
     }

fac6e70b55ff9e2c1f66c869853d282dcce75521
Ashwin Valento <ashwinvalento@gmail.com>
2014-02-06 03:34:25 +0530

    -> Updated database design

--- src/run/DBConnect.java
+++ src/run/DBConnect.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public final class DBConnect {
+public final class DBConnect implements DBInterface {
 
     //SQLITE database Drivers
     public static String driver = "org.sqlite.JDBC";
@@ -31,62 +31,19 @@ public final class DBConnect {
         }
 
         Statement stmt, stmt1;
-        String query1 = "CREATE TABLE RESULTTABLE\n"
-                + "(\n"
-                + "	USN VARCHAR(12),\n"
-                + "	NAME VARCHAR(50),\n"
-                + "	SUB1_INT INTEGER,\n"
-                + "	SUB1_EXT INTEGER,\n"
-                + "	SUB1_TOT INTEGER,\n"
-                + "	SUB1_RES CHAR(1),\n"
-                + "	SUB2_INT INTEGER,\n"
-                + "	SUB2_EXT INTEGER,\n"
-                + "	SUB2_TOT INTEGER,\n"
-                + "	SUB2_RES CHAR(1),\n"
-                + "	SUB3_INT INTEGER,\n"
-                + "	SUB3_EXT INTEGER,\n"
-                + "	SUB3_TOT INTEGER,\n"
-                + "	SUB3_RES CHAR(1),\n"
-                + "	SUB4_INT INTEGER,\n"
-                + "	SUB4_EXT INTEGER,\n"
-                + "	SUB4_TOT INTEGER,\n"
-                + "	SUB4_RES CHAR(1),\n"
-                + "	SUB5_INT INTEGER,\n"
-                + "	SUB5_EXT INTEGER,\n"
-                + "	SUB5_TOT INTEGER,\n"
-                + "	SUB5_RES CHAR(1),\n"
-                + "	SUB6_INT INTEGER,\n"
-                + "	SUB6_EXT INTEGER,\n"
-                + "	SUB6_TOT INTEGER,\n"
-                + "	SUB6_RES CHAR(1),\n"
-                + "	SUB7_INT INTEGER,\n"
-                + "	SUB7_EXT INTEGER,\n"
-                + "	SUB7_TOT INTEGER,\n"
-                + "	SUB7_RES CHAR(1),\n"
-                + "	SUB8_INT INTEGER,\n"
-                + "	SUB8_EXT INTEGER,\n"
-                + "	SUB8_TOT INTEGER,\n"
-                + "	SUB8_RES CHAR(1),\n"
-                + "	TOTAL INTEGER,\n"
-                + "	MARKCLASS VARCHAR(50)\n"
-                + ")";
 
-        String query2 = "CREATE TABLE \"SUBJECTTABLE\" (\"subjectname\" VARCHAR DEFAULT \"SUBJECT\" )";
-
-        String query3 = "CREATE TABLE \"sub_table\" (\"usn\" VARCHAR NOT NULL , \"subname\" VARCHAR NOT NULL , \"internal\" INTEGER, \"external\" INTEGER, \"total\" INTEGER, \"result\" CHAR, PRIMARY KEY (\"usn\", \"subname\"))";
-        String query4 = "CREATE TABLE \"main\" (\"usn\" VARCHAR, \"name \" VARCHAR, \"f_total\" INTEGER, \"f_result\" VARCHAR)";
+        String query1 = "CREATE TABLE " + SUBJECT_DETAILS + " ('" + SUB_USN + "' VARCHAR NOT NULL , '" + SUB_SUBNAME + "' VARCHAR NOT NULL , '" + SUB_INTERNAL + "' INTEGER, '" + SUB_EXTERNAL + "' INTEGER, '" + SUB_TOTAL + "' INTEGER, '" + SUB_RESULT + "' CHAR, PRIMARY KEY (" + SUB_USN + ", " + SUB_SUBNAME + "))";
+        String query2 = "CREATE TABLE " + STUDENT_DETAILS + " ('" + ST_USN + "' VARCHAR, '" + ST_NAME + "' VARCHAR, '" + ST_TOTAL + "' INTEGER, '" + ST_RESULT + "' VARCHAR)";
         try {
             stmt = connection.createStatement();
             stmt1 = connection.createStatement();
 
-            stmt.executeUpdate(query3);
+            stmt.executeUpdate(query1);
             System.out.println("Result Table created");
-            stmt1.executeUpdate(query4);
+            stmt1.executeUpdate(query2);
             System.out.println("Subject table created");
-
         } catch (SQLException ex) {
-            //Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
-
+            // Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Table already exists .");
         }
 

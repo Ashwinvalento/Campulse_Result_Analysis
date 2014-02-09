e6eee77b862f6172f6d10dde813da2c0f987830a
Ashwin Valento <ashwinvalento@gmail.com>
2014-02-09 20:47:46 +0530

    -> table for back paper added

--- src/run/DBConnect.java
+++ src/run/DBConnect.java
@@ -33,18 +33,23 @@ public final class DBConnect implements DBInterface {
             Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        Statement stmt, stmt1;
+        Statement stmt, stmt1, stmt2;
 
         String query1 = "CREATE TABLE " + SUBJECT_DETAILS + " ('" + SUB_USN + "' VARCHAR NOT NULL , '" + SUB_SUBNAME + "' VARCHAR NOT NULL , '" + SUB_INTERNAL + "' INTEGER, '" + SUB_EXTERNAL + "' INTEGER, '" + SUB_TOTAL + "' INTEGER, '" + SUB_RESULT + "' CHAR, PRIMARY KEY (" + SUB_USN + ", " + SUB_SUBNAME + "))";
         String query2 = "CREATE TABLE " + STUDENT_DETAILS + " ('" + ST_USN + "' VARCHAR, '" + ST_NAME + "' VARCHAR, '" + ST_TOTAL + "' INTEGER, '" + ST_RESULT + "' VARCHAR, PRIMARY KEY (" + ST_USN + "))";
+        String query3 = "CREATE TABLE " + BACKSUB_DETAILS + " ('" + SUB_USN + "' VARCHAR NOT NULL , '" + SUB_SUBNAME + "' VARCHAR NOT NULL , '" + SUB_INTERNAL + "' INTEGER, '" + SUB_EXTERNAL + "' INTEGER, '" + SUB_TOTAL + "' INTEGER, '" + SUB_RESULT + "' CHAR, PRIMARY KEY (" + SUB_USN + ", " + SUB_SUBNAME + "))";
+
         try {
             stmt = connection.createStatement();
             stmt1 = connection.createStatement();
+            stmt2 = connection.createStatement();
 
             stmt.executeUpdate(query1);
             System.out.println("subject_details created");
             stmt1.executeUpdate(query2);
             System.out.println("student_details created");
+            stmt2.executeUpdate(query3);
+            System.out.println("back subject created");
             Forms.MainForm.log("Initializing Database : Successfull");
         } catch (SQLException ex) {
             //Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);

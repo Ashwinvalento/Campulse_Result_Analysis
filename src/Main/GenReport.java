0e81f645560840593c49fdd2a1ce53b7c99fba66
neo <neo@matrix>
2014-02-09 22:12:26 +0530

    Report page updated. labels provide more info

--- src/Main/GenReport.java
+++ src/Main/GenReport.java
@@ -34,6 +34,43 @@ public class GenReport implements DBInterface {
     private String subName;
     private int subjectSize;
     Connection con;
+    int classFCD;
+    int classFC;
+    int classSC;
+    int classFail;
+
+    public int getClassFCD() {
+        return classFCD;
+    }
+
+    public void setClassFCD(int classFCD) {
+        this.classFCD = classFCD;
+    }
+
+    public int getClassFC() {
+        return classFC;
+    }
+
+    public void setClassFC(int classFC) {
+        this.classFC = classFC;
+    }
+
+    public int getClassSC() {
+        return classSC;
+    }
+
+    public void setClassSC(int classSC) {
+        this.classSC = classSC;
+    }
+
+    public int getClassFail() {
+        return classFail;
+    }
+
+    public void setClassFail(int classFail) {
+        this.classFail = classFail;
+    }
+    
 
     public DefaultTableModel fillReportTable() {
         con = DBConnect.connection;
@@ -81,24 +118,28 @@ public class GenReport implements DBInterface {
         }
         return model;
     }
+    
+    public void getAllLabelValues() {
+        setClassFCD(getClassFCDCount());
+        setClassFC(getClassFirstCount());
+        setClassSC(getClassSecondCount());
+        setClassFail(getClassFailCount());
+    }
 
     private int getFailCount(String subName) {
         int FailCount = 0;
 
         ResultSet rs = null;
         failQuery = "SELECT COUNT(*) FROM " + SUBJECT_DETAILS + " WHERE " + SUB_SUBNAME + "='" + subName + "' AND " + SUB_RESULT + "='F'";
-        System.out.println("fail Query :  " + failQuery);
         Statement stmt;
         try {
             stmt = con.createStatement();
             rs = stmt.executeQuery(failQuery);
             if (rs.next()) {
                 FailCount = rs.getInt(1);
-                System.out.println("failCount " + FailCount);
             }
         } catch (SQLException ex) {
-            System.out.println("error : " + ex.getMessage());
-            // Logger.getLogger(GenReport.class.getName()).log(Level.SEVERE, null, ex);
+             Logger.getLogger(GenReport.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         return FailCount;
@@ -201,5 +242,86 @@ public class GenReport implements DBInterface {
         }
 
     }
+
+    private int getClassFCDCount() {
+        int fcdCount = 0;
+
+        ResultSet rs = null;
+        String Query = "SELECT COUNT(*) FROM " + STUDENT_DETAILS + " WHERE " + ST_RESULT + "='FIRST CLASS WITH DISTINCTION'";
+        
+        Statement stmt;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(Query);
+            if (rs.next()) {
+                fcdCount = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GenReport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return fcdCount;
+    }
+
+    private int getClassFirstCount() {
+        int fcCount = 0;
+
+        ResultSet rs = null;
+        String Query = "SELECT COUNT(*) FROM " + STUDENT_DETAILS + " WHERE " + ST_RESULT + "='FIRST CLASS'";
+        System.out.println(Query);
+        Statement stmt;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(Query);
+            if (rs.next()) {
+                fcCount = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GenReport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return fcCount;
+    }
+
+    private int getClassSecondCount() {
+        int scCount = 0;
+
+        ResultSet rs = null;
+        String Query;
+        Query = "SELECT COUNT(*) FROM " + STUDENT_DETAILS + " WHERE " + ST_RESULT + "='SECOND CLASS'";
+        Statement stmt;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(Query);
+            if (rs.next()) {
+                scCount = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GenReport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return scCount;    }
+
+    private int getClassFailCount() {
+        int failCount = 0;
+
+        ResultSet rs = null;
+        String Query;
+        Query = "SELECT COUNT(*) FROM " + STUDENT_DETAILS + " WHERE " + ST_RESULT + "='FAIL'";
+        Statement stmt;
+        try {
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(Query);
+            if (rs.next()) {
+                failCount = rs.getInt(1);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GenReport.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return failCount;
+    }
+
+    
 
 }

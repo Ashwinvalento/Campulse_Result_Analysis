a7720832d9cf8405f27ca9adf8df50d7667299e0
Ashwin Valento <ashwinvalento@gmail.com>
2014-02-09 16:55:40 +0530

    -> Result fetching made completly dynamic . [check if back is not required].

--- src/Main/resultFetch_1.java
+++ src/Main/resultFetch_1.java
@@ -1,5 +1,6 @@
 package Main;
 
+import Forms.MainForm;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -18,21 +19,23 @@ import org.jsoup.select.Elements;
 public class resultFetch_1 {
 
     int marks[][] = new int[20][3];
-    String res[] = new String[8];
+    String res[] = new String[20];
     String subjects[] = new String[20];
     String totalmarks = null;
     String mk;
     String name;
-    int subs = 0;
+    int subs;
 
     public resultFetch_1() {
         setProxy();
+        subs = 0;
     }
 
     public boolean FetchTheresult(String usn, int sem) {
         Document doc;
+        subs = 0;
         //String url = "http://results.vtualerts.com/get_res.php?usn=" + usn ;
-        String url = "http://results.vtualerts.com/get_res.php?usn=" + usn+ "&sem=" + sem;
+        String url = "http://results.vtualerts.com/get_res.php?usn=" + usn + "&sem=" + sem;
         if (Forms.EnterUsnForm.sem > 0 && Forms.EnterUsnForm.sem < 9) {
             url = "http://results.vtualerts.com/get_res.php?usn=" + usn + "&sem=" + sem;
         }
@@ -40,44 +43,106 @@ public class resultFetch_1 {
         try {
             doc = Jsoup.connect(url).userAgent("Mozilla").get();
             Element StdName = doc.select("div").select("B:eq(0)").first();
-            name = StdName.toString().split(">")[1].split(Pattern.quote("("))[0];
+            name = StdName.text().split(Pattern.quote("("))[0];
 
             Element Stdsem = doc.select("div").get(1).select("B").get(2);
-            System.out.println(Stdsem.text());
+            //    System.out.println(Stdsem.text());
 
+            Element markclass = doc.select("table:eq(1)").select("td:eq(3)").select("b:eq(0)").first();
+            System.out.println("mark class :" + markclass.text().split("Result:  ")[1] + ":");
+            mk = markclass.text().split("Result:  ")[1];
             if (sem < Integer.parseInt(Stdsem.text())) {
                 System.out.println("Year out");
                 return false;
             }
+
+            // Static fetching 
             /* System.out.println("name is : " + name);
              if (name.equalsIgnoreCase("Semester:</b")) {
              System.out.println("Year out");
 
              return false;
              }*/
-            Element markclass = doc.select("table:eq(1)").select("td:eq(3)").select("b:eq(0)").first();
-            mk = markclass.toString().split(";")[2].split("<")[0];
-            Element firstTableMarks = doc.select("table:eq(3)").first();
+            /* Element markclass = doc.select("table:eq(1)").select("td:eq(3)").select("b:eq(0)").first();
+             System.out.println("mark class :" + markclass.text() + ":");
+             mk = trimspace(markclass.text().split("Result:  ")[1]);
+             Element firstTableMarks = doc.select("table:eq(3)").first();
 
-            Element tmtbody = firstTableMarks.select("tbody").first();
+             Element tmtbody = firstTableMarks.select("tbody").first();
 
-            if (sem == 8) {
-                subs = 6;
-            } else {
-                subs = 8;
+             if (sem == 8) {
+             subs = 6;
+             } else {
+             subs = 8;
+             }
+
+             for (int i = 0; i < subs; i++) {
+             String whichTr = "tr:eq(" + (i + 1) + ")";
+             subjects[i] = tmtbody.select(whichTr).first().child(0).text();
+             marks[i][0] = Integer.parseInt(tmtbody.select(whichTr).first().child(1).text());
+             marks[i][1] = Integer.parseInt(tmtbody.select(whichTr).first().child(2).text());
+             marks[i][2] = Integer.parseInt(tmtbody.select(whichTr).first().child(3).text());
+             res[i] = tmtbody.select(whichTr).first().child(4).text();
+
+             Element totMks = doc.select("table:eq(4)").select("td:eq(3)").first();
+             totalmarks = totMks.toString().split(" ")[1];
+             }
+             */
+// dynamic result 
+            System.out.println("Dynameic result");
+            int TableNo = 0, skipTable = 0, skipRow = 0; // skip the google adds table and name and sem table
+            for (Element table : doc.select("table")) {
+                TableNo++;
+                if (TableNo == 4) {
+                    String total = table.select("tr").select("td").text();
+                    if (total.startsWith("Total Marks:")) {
+                        totalmarks = total.split("Total Marks: ")[1].split(" ")[0];
+                        System.out.println("total =" + totalmarks + ":");
+                    } else {
+                        totalmarks = "0";
+                    }
+
+                }
+                System.out.println("table no :" + TableNo);
+                if (skipTable < 2) {
+                    skipTable++;
+                    System.out.println("skipped");
+                    continue;
+                }
+
+                System.out.println("--------------------\nNEW TABLE\n");
+                for (Element row : table.select("tr")) {
+                    if (skipRow < 1) {
+                        System.out.println("1 row skipped");
+                        skipRow++;
+                        continue;
+                    }
+                    System.out.println("\nNEW ROW\n");
+                    Elements tds = row.select("td");
+                    if (tds.size() > 0) {
+                        System.out.println("Subject = " + tds.get(0).select("i").text());
+                        subjects[subs] = tds.get(0).select("i").text();
+
+                        System.out.println("Internal = " + tds.get(1).select("td").text());
+                        marks[subs][0] = Integer.parseInt(tds.get(1).select("td").text());
+
+                        System.out.println("External = " + tds.get(2).select("td").text());
+                        marks[subs][1] = Integer.parseInt(tds.get(2).select("td").text());
+
+                        System.out.println("Total = " + tds.get(3).select("td").text());
+                        marks[subs][2] = Integer.parseInt(tds.get(3).select("td").text());
+
+                        System.out.println("Result = " + tds.get(4).select("td").text());
+                        res[subs++] = tds.get(4).select("td").text();
+
+                    }
+                }
+                skipTable = 1;
+                skipRow = 0;
+                System.out.println("--------------------\n");
             }
-            for (int i = 0; i < subs; i++) {
-                String whichTr = "tr:eq(" + (i + 1) + ")";
-                subjects[i] = tmtbody.select(whichTr).first().child(0).text();
-                marks[i][0] = Integer.parseInt(tmtbody.select(whichTr).first().child(1).text());
-                marks[i][1] = Integer.parseInt(tmtbody.select(whichTr).first().child(2).text());
-                marks[i][2] = Integer.parseInt(tmtbody.select(whichTr).first().child(3).text());
-                res[i] = tmtbody.select(whichTr).first().child(4).text();
-
-                Element totMks = doc.select("table:eq(4)").select("td:eq(3)").first();
-                totalmarks = totMks.toString().split(" ")[1];
-            }
-
+            MainForm.DF.setStatus(usn, "Success");
+            MainForm.log(usn + " Download : Success");
         } catch (Exception e) {
             return checkIfResultisAvailable(usn, sem);
             /*
@@ -86,7 +151,10 @@ public class resultFetch_1 {
              System.out.println("fetch failed for " + usn);
              MainForm.DF.setStatus(usn, "failed");*/
             ///return false;//if result is not found
+
+//            e.printStackTrace();
         }
+
         return true;//result is found
     }
 
@@ -150,7 +218,7 @@ public class resultFetch_1 {
 
     private boolean checkIfResultisAvailable(String usn, int sem) {
         Document doc;
-        //MainForm.log(usn + " Checking if result is available");
+        MainForm.log(usn + " Checking if result is available");
         System.out.println("Checking if result is available");
         String resultNotAvail = "<h3>Sorry Results are not yet available for this university seat number<br />or it might not be a valid university seat number</h3>";
         String url = "http://results.vtualerts.com/get_res.php?usn=" + usn;
@@ -159,21 +227,20 @@ public class resultFetch_1 {
         }
         try {
             doc = Jsoup.connect(url).userAgent("Mozilla").timeout(Forms.MainForm.timeout * 1000).get();
-            System.out.println("comes to try result available");
             Element StdName = doc.select("center").first().child(0);
             System.out.println(StdName);
             if (StdName.toString().equals(resultNotAvail)) {
-                //  MainForm.log(usn + " Download : Result not available");
-                //  MainForm.DF.setStatus(usn, "Not Available");
+                  MainForm.log(usn + " Download : Result not available");
+                  MainForm.DF.setStatus(usn, "Not Available");
             }
 
         } catch (Exception e) {
-            return checkIfHeHasBackPaper(usn, sem);
-            /*MainForm.RetryList.add(usn);
-             System.out.println("usn added for retry !");
-             System.out.println("fetch failed for " + usn);
-             MainForm.DF.setStatus(usn, "failed");
-             */
+            // return checkIfHeHasBackPaper(usn, sem);
+            MainForm.RetryList.add(usn);
+            System.out.println("usn added for retry !");
+            System.out.println("fetch failed for " + usn);
+            MainForm.DF.setStatus(usn, "failed");
+
         }
         return false;
     }
@@ -253,6 +320,8 @@ public class resultFetch_1 {
 
     public static void main(String[] args) {
         resultFetch_1 rs = new resultFetch_1();
-        rs.FetchTheresult("4PA10CS072", 6);
+        rs.FetchTheresult("4PA12is001", 3);
+        // rs.FetchTheresult("4PA10CS013", 7);
     }
+
 }
